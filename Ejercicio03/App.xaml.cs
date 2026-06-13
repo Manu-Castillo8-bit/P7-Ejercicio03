7c32a033f6a196255e793bda733e0abfa47f8fb1
Manuel <ernesto.ccastilloguerra2@gmail.com>
2026-06-12 21:14:33 -0600

    Paso 22

--- Ejercicio03/App.xaml.cs
+++ Ejercicio03/App.xaml.cs
@@ -7,11 +7,9 @@ namespace Ejercicio03
         public App()
         {
             InitializeComponent();
+            MainPage = new MainPage();
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+       
     }
 }
